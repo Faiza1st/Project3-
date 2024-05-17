6716fef8dd67a30a5b1c6126f497d3d08f46c5dd
Faiza1st <faixa.faisal@gmail.com>
2024-05-17 22:49:59 +1000

    package.json in server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import {v2 as cloudinary} from 'cloudinary';
