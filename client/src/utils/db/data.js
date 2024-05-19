14f80f650f1d34677b032fe8585ce2833deb908d
Faiza1st <faixa.faisal@gmail.com>
2024-05-20 02:57:57 +1000

    login page

--- client/src/utils/db/data.js
+++ client/src/utils/db/data.js
@@ -7,8 +7,6 @@ export const POSTS = [
             username: "oceanlover22",
             profileImg: "/avatars/boy1.png",
             fullName: "Marina Fisher",
-            course: "Marine Biology",
-            year: "Sophomore"
         },
         comments: [
             {
@@ -18,13 +16,10 @@ export const POSTS = [
                     username: "beachcomber45",
                     profileImg: "/avatars/boy2.png",
                     fullName: "Sandy Shore",
-                    course: "Environmental Science",
-                    year: "Freshman"
                 },
             },
         ],
         likes: ["1234abc", "5678def", "9012ghi"],
-        timestamp: "2024-05-18T14:23:00Z",
     },
     {
         _id: "2",
@@ -33,12 +28,9 @@ export const POSTS = [
             username: "chefextraordinaire",
             profileImg: "/avatars/boy2.png",
             fullName: "Gordon Ramsay",
-            course: "Culinary Arts",
-            year: "Senior"
         },
         comments: [],
         likes: ["abcd123", "efgh456"],
-        timestamp: "2024-05-18T12:47:00Z",
     },
     {
         _id: "3",
@@ -48,8 +40,6 @@ export const POSTS = [
             username: "outdooradventurer",
             profileImg: "/avatars/girl2.png",
             fullName: "Alex Woods",
-            course: "Outdoor Recreation",
-            year: "Junior"
         },
         comments: [
             {
@@ -59,8 +49,6 @@ export const POSTS = [
                     username: "naturelover123",
                     profileImg: "/avatars/boy3.png",
                     fullName: "Emma Green",
-                    course: "Botany",
-                    year: "Sophomore"
                 },
             },
             {
@@ -70,13 +58,10 @@ export const POSTS = [
                     username: "trailblazer89",
                     profileImg: "/avatars/girl1.png",
                     fullName: "Jake Summit",
-                    course: "Geology",
-                    year: "Junior"
                 },
             },
         ],
         likes: ["wxyz789"],
-        timestamp: "2024-05-17T17:35:00Z",
     },
 ];
 
@@ -86,47 +71,35 @@ export const USERS_FOR_RIGHT_PANEL = [
         fullName: "Marina Fisher",
         username: "oceanlover22",
         profileImg: "/avatars/boy1.png",
-        course: "Marine Biology",
-        year: "Sophomore"
     },
     {
         _id: "2",
         fullName: "Sandy Shore",
         username: "beachcomber45",
         profileImg: "/avatars/boy2.png",
-        course: "Environmental Science",
-        year: "Freshman"
     },
     {
         _id: "3",
         fullName: "Gordon Ramsay",
         username: "chefextraordinaire",
         profileImg: "/avatars/boy3.png",
-        course: "Culinary Arts",
-        year: "Senior"
     },
     {
         _id: "4",
         fullName: "Alex Woods",
         username: "outdooradventurer",
         profileImg: "/avatars/girl1.png",
-        course: "Outdoor Recreation",
-        year: "Junior"
     },
     {
         _id: "5",
         fullName: "Emma Green",
         username: "naturelover123",
         profileImg: "/avatars/girl2.png",
-        course: "Botany",
-        year: "Sophomore"
     },
     {
         _id: "6",
         fullName: "Jake Summit",
         username: "trailblazer89",
         profileImg: "/avatars/girl3.png",
-        course: "Geology",
-        year: "Junior"
     },
 ];
