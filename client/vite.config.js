269ea9e3370a10a28f81660a61aeb50a1d45c8b5
Faiza1st <faixa.faisal@gmail.com>
2024-05-20 23:54:23 +1000

    Heroko

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const port = process.env.PORT || 4050 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
 		port: 4000,
+		
 		proxy: {
 			"/api": {
-				target: "http://localhost:4050/",
+				target: `http://localhost:${port}/`,
 				changeOrigin: true,
 			},
 		},
