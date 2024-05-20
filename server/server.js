4afc49734d6616d658f03a002d52a71069ab92f7
Faiza1st <faixa.faisal@gmail.com>
2024-05-21 00:16:53 +1000

    Heroko

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+import path from "path";
+import.meta.dirname; 
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -10,6 +12,9 @@ import postRoutes from "./routes/postRoutes.js";
 import notificationRoutes from "./routes/notificationRoutes.js";
 
 import mongoDb from "./db/mongodb.js";
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 dotenv.config();
 
@@ -24,12 +29,19 @@ const PORT = process.env.PORT || 4050;
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:4000", // frontend URL
-    credentials: true, // Allow credentials (cookies) to be included
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:4000", // frontend URL
+//     credentials: true, // Allow credentials (cookies) to be included
+//   })
+//);
+// if (process.env.NODE_ENV === 'production') {
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('*', (req, res) => {
+res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
